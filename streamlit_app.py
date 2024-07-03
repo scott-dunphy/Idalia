8d1e22161e6b44d0629d352f85ee844bc938de44
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-07-03 13:44:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ import requests
 def address_to_lat_lon(address):
     url = f"https://nominatim.openstreetmap.org/search?q={address}&format=jsonv2"
     headers = {
-        'User-Agent': 'YourAppName/1.0 (your.email@example.com)'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
